27a9ebc7368fbf1e94b277f6e5c01fe1ba9dd356
Alex <reyesgx@gmail.om>
2018-10-01 19:16:50 -0700

    form validation with slides features

--- src/pages/wizard/wizard.ts
+++ src/pages/wizard/wizard.ts
@@ -20,31 +20,53 @@ export class WizardPage {
   constructor(public navCtrl: NavController, private formBuilder: FormBuilder, public navParams: NavParams) {
     this.todo = this.formBuilder.group({
       injured: ["injured", Validators.required],
-     
       EnlistingDate: ['', Validators.required],
       branch: ['' ]
     });
-
   }
-
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad WizardPage');
-    
+    console.log('Number of slides are :dcdcd');
+    this.lockNextSlide()
   }
+  nextButton: boolean = true;
+
   logForm(){
+    console.log(this.todo.value)
+ }
 
-     console.log(this.todo.value)
+  shouldLockSwipeToNext= false;
+
+  slideChanged() {
+    let index = this.slides.getActiveIndex() 
+    console.log(index);
+    if(index == 5) {
+      
+    this.shouldLockSwipeToNext = true;
+    this.lockNextSlide()
+    } else {
+      this.shouldLockSwipeToNext = false;
+      this.lockNextSlide()
+    }
   }
 
-
+  lockNextSlide(){
+     ; //  shouldLockSwipeToNext can be either true/false
+    this.slides.lockSwipeToNext(this.shouldLockSwipeToNext);
+    console.log('Number of slides are :dcdcd'); 
+  }
+ 
   //when navigating to the new slide when user clicks submit 
   onSubmit() {  
-      this.slides.slideTo(6, 500);
+    this.shouldLockSwipeToNext = false;
+    this.lockNextSlide()
+    this.next();
 
     }
-    
-next() {
-  this.slides.slideTo(6, 500);
-}
+
+  next() {
+    this.slides.slideNext(500);
+  }
 
 }
