a3aac2aa96d826ac0fbbf805b0103d4484558385
Alex <reyesgx@gmail.om>
2018-10-06 15:56:23 -0700

    new design for the questionnaire slide, inclding css,html,ts, with pop-ups

--- src/pages/wizard/wizard.ts
+++ src/pages/wizard/wizard.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
-import { NavController, NavParams, Slides } from 'ionic-angular';
-import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { NavController, NavParams, Slides, AlertController} from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 /**
  * Generated class for the WizardPage page.
@@ -17,8 +17,12 @@ export class WizardPage {
 
   private todo : FormGroup;
   @ViewChild(Slides) slides: Slides;
-  constructor(public navCtrl: NavController, private formBuilder: FormBuilder, public navParams: NavParams) {
-    this.todo = this.formBuilder.group({
+
+  constructor( public alertCtrl: AlertController, 
+              public navCtrl: NavController,
+              private formBuilder: FormBuilder, 
+              public navParams: NavParams ) {
+      this.todo = this.formBuilder.group({
       injured: ["injured", Validators.required],
       EnlistingDate: ['', Validators.required],
       branch: ['' ]
@@ -28,28 +32,100 @@ export class WizardPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad WizardPage');
     console.log('Number of slides are :dcdcd');
-    this.lockNextSlide()
+   // this.lockNextSlide()
   }
-  nextButton: boolean = true;
 
+//radio alert for qestionare 1: marine branches
+  showRadio() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Lightsaber color');
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Army',
+      value: 'army',
+      checked: false
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Air Force',
+      value: 'air force',
+      checked: false
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Coast Guards',
+      value: 'coast guards',
+      checked: false
+    });
+   
+
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'OK',
+      handler: data => {
+      console.log("OK", data);
+      }
+    });
+    alert.present();
+  }
+  // end of 1st alert
+  showRadioRank() {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('Lightsaber color');
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Private',
+      value: 'private',
+      checked: false
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Sergent',
+      value: 'sergent',
+      checked: false
+    });
+
+    alert.addInput({
+      type: 'radio',
+      label: 'First Sergent',
+      value: 'first sergent',
+      checked: false
+    });
+
+    alert.addButton('Cancel');
+    alert.addButton({
+      text: 'OK',
+      handler: data => {
+      console.log("OK", data);
+      }
+    });
+    alert.present();
+  }
+  
   logForm(){
     console.log(this.todo.value)
- }
+  }
 
+  nextButton: boolean = true;
   shouldLockSwipeToNext= false;
 
-  slideChanged() {
-    let index = this.slides.getActiveIndex() 
-    console.log(index);
-    if(index == 5) {
+  // slideChanged() {
+  //   let index = this.slides.getActiveIndex() 
+  //   console.log(index);
+  //   if(index == 5) {
       
-    this.shouldLockSwipeToNext = true;
-    this.lockNextSlide()
-    } else {
-      this.shouldLockSwipeToNext = false;
-      this.lockNextSlide()
-    }
-  }
+  //   this.shouldLockSwipeToNext = true;
+  //   this.lockNextSlide()
+  //   } else {
+  //     this.shouldLockSwipeToNext = false;
+  //     this.lockNextSlide()
+  //   }
+  // }
 
   lockNextSlide(){
      ; //  shouldLockSwipeToNext can be either true/false
