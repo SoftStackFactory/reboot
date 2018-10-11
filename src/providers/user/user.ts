8b4569b4cb297108a9f8e12349cd97dbca1fe73f
awstin17 <austinwiese.acw@gmail.com>
2018-10-10 21:21:29 -0700

    fixed import issue

--- src/providers/user/user.ts
+++ src/providers/user/user.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import  { ENV }  from  '@app /env';
+import  {  ENV  }  from  '@app/env';
 
 /*
   Generated class for the UserProvider provider.
