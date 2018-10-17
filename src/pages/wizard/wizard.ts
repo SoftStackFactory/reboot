46fbf6082f67c7a97497326131eafce214bb54d6
Alex <reyesgx@gmail.om>
2018-10-17 13:50:21 -0700

    clean code, made comments to explain code, and fixed some html text

--- src/pages/wizard/wizard.ts
+++ src/pages/wizard/wizard.ts
@@ -45,39 +45,43 @@ export class WizardPage {
       this.slideChanged() 
       }, 300 );
   };
- vetValue: string = "";
- SeparationQuestion: string = ""; 
- disabilityQValue: string = "";
+  vetValue: string = "";
+  SeparationQuestion: string = ""; 
+  disabilityQValue: string = "";
+
+  disableSwipe() {
+    this.nextButton = true;
+    this.shouldLockSwipeToNext = true;
+  }
  
+  eneableSwipe() {
+    this.nextButton = false;
+    this.shouldLockSwipeToNext = false;
+  }
+  //callled when status changes for forms
   statusChangeFunct(valid: boolean) {
-    console.log("on new funct")
     if(valid == true) {
-      console.log(valid, "newFunct-valid")
-      this.nextButton = false;
-      this.shouldLockSwipeToNext = false;
+      this.eneableSwipe()
     }else if( valid == false) {
-      console.log(valid, "newFunct-not valid")
-      this.nextButton = true;
-      this.shouldLockSwipeToNext = true;
+      this.disableSwipe()
     } 
     this.lockNextSlide()
   }
- 
+  //declare firstForm; set contol names; set validators
   firstFormFunct() {
     this.firstForm = this.formBuilder.group({
       branch:  ['', Validators.compose([Validators.required])],
       vetOrActive: ['', Validators.compose([Validators.required])],
       separationDate: ['', Validators.compose([Validators.required])],
       disability: ['', Validators.compose([Validators.required])],
-      percentQuestion: ["",]
+      percentQuestion: ["",  ]
     });
-
+    //observable called when status of firstForm changes => calls function that enables or disabled swipe if conditions are met
     this.firstForm.statusChanges
       .subscribe(val => {
-        console.log("statusChanged1#1", val)
         this.statusChangeFunct(this.firstForm.valid)
       })
-
+    //when value changes make separation data appear and change wording depending on conditions
     this.firstForm.controls.vetOrActive.valueChanges
       .subscribe( val =>{
         if(val =="Active") {
@@ -88,14 +92,13 @@ export class WizardPage {
           this.SeparationQuestion = "";
         }
       this.vetValue = val;
-      console.log("vetOrAct.ValueChanges", this.firstForm.valid)
-       })
-      
+      })
+    //when value changes for disability question change validator requirements for percentQuestion depending on condition
     this.firstForm.controls.disability.valueChanges
       .subscribe( data => {
         const percentQuestion = this.firstForm.get('percentQuestion')
         if(data == "disability") {  
-          percentQuestion.setValidators(Validators.compose([ Validators.maxLength(3), Validators.required, Validators.pattern('^[1-9]$|^[1-9][0-9]$|^(100)$')]));
+          percentQuestion.setValidators( Validators.compose([Validators.maxLength(3),Validators.required ,Validators.pattern('^[1-9]$|^[1-9][0-9]$|^(1/00)$')]));
         }else {           
           percentQuestion.clearValidators() 
         }
@@ -103,66 +106,59 @@ export class WizardPage {
         this.disabilityQValue = data; 
       })
   };
- 
+ //
   employedAnswer: string= "";
+  //declare SecondForm; set contol names; set validators
   secondFormFunct(){
     this.secondForm = this.formBuilder.group({
       employment: ['', Validators.compose([Validators.required])],
       lastEmployed: [''],
       marriage: ['', Validators.compose([Validators.required])],
     });
-    //1 do a [] bracked in "" to have only one subscribe stautsChange
-    //have only one selectOptions
-
+    //when status changes call function to lock or unlock swipe dependign if form is valid
     this.secondForm.statusChanges
       .subscribe(val => {
         this.statusChangeFunct(this.secondForm.valid)
-        console.log( "statusChanges2#2")
       })
-//if error 'infinite loop comment out .upadateValueAndValidity()'
+  //if error 'infinite loop' comment out .upadateValueAndValidity()'
+    //when value changes for employment question, set validators for lastEmployment question and update form depending if marked as unemployed
     this.secondForm.controls.employment.valueChanges
       .subscribe( data => {
         const employmentQuestion = this.secondForm.get('lastEmployed')
         if(data == "Unemployed") { 
-          console.log("ValueChanges-#1", data)
           employmentQuestion.setValidators(Validators.compose([ Validators.required]));
         }else {
           employmentQuestion.clearValidators() 
-          console.log("ValueChanges#2", data) 
         }
-        console.log(this.secondForm.valid, "ValueChanges#3")
-        employmentQuestion.updateValueAndValidity()
-        console.log(this.secondForm.valid, "ValueChanges#4")
+        employmentQuestion.updateValueAndValidity();
         this.employedAnswer = data; 
       })
   };
 
+  //declare thirdForm; set contol names; set validators
   thirdFormFunct() {
     this.thirdForm = this.formBuilder.group({
       rank: ["", Validators.compose([ Validators.required, Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*')])],
       MOS: ["", Validators.compose([ Validators.maxLength(9), Validators.required,  Validators.pattern('[0-9]+')]) ]
     });
   };
-  // "if slide is questionnaire, then block slide-to-next option"
+  // called when users click on nav bar 'next' button; only enabled when forms are valid
   next() {
     this.slides.slideNext(500);
   }
+  //shouldLockSwipeToNext variable can be either true/false depending on condition
   lockNextSlide(){
-    //  shouldLockSwipeToNext can be either true/false
     this.slides.lockSwipeToNext(this.shouldLockSwipeToNext);
   }
-
+  //the logic that determines if slide should be lockSwiped 
   nextButton: boolean = false;
   shouldLockSwipeToNext: boolean = false;
   slideChanged() {
     let index = this.slides.realIndex; 
-    console.log(index);
     if((index == 5 && !this.firstForm.valid) || (index == 6 && !this.secondForm.valid) || (index == 8 ) || (index == 7))  {
-      this.nextButton = true;
-      this.shouldLockSwipeToNext = true;
+      this.disableSwipe()
     }else {
-        this.nextButton = false;
-        this.shouldLockSwipeToNext = false;
+      this.eneableSwipe()
     }
     this.lockNextSlide()
   }
@@ -210,6 +206,7 @@ export class WizardPage {
       militaryRank: this.thirdForm.value.rank,
       MOS: this.thirdForm.value.MOS
     }
+    console.log(userData)
     this.shouldLockSwipeToNext = false;
     this.lockNextSlide()
     this.next();
