35ff817fc5d005c51ec6a46c15580c230e8e704c
awstin17 <austinwiese.acw@gmail.com>
2018-10-26 11:21:26 -0700

    Changed http request to get chart history based on userId

--- src/providers/chart/chart.ts
+++ src/providers/chart/chart.ts
@@ -1,16 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import  {  ENV  }  from  '@app/env';
+
 @Injectable()
 export class ChartProvider {
 
   data: any = [0, 0, 0, 0, 0, 0, 0, 0]
+  requestUrl: string = ENV.url
 
   constructor(public http: HttpClient) {
     console.log('Hello ChartProvider Provider');
   }
 
   getHistory() {
-    return this.http.get('https://reboot-ssf.herokuapp.com/api/appUsers/5bce484f8e47330014efe009/charts')
+    return this.http.get(this.requestUrl + '/appUsers/' + sessionStorage.getItem('userId') + '/charts')
   }
 }
