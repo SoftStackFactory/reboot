9e374f30646a5659479ac58507cf39e0fb1e732d
BrettWray <brett@brettwray.com>
2018-11-20 16:09:30 -0800

    Made the history chart function dynamic by scripting the data model of the chart

--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -20,35 +20,54 @@ export class HistoryPage {
     @ViewChild('lineChart') lineChart;
 
   historyPage: any = 'historyPage';
-  areas = ["Career", "Finance", "Personal \n Growth", "Health", "Family", "Relationships", "Social life", "Attitude"]
-  
+  labels  = ["Career", "Finance", "Personal \n Growth", "Health", "Family", "Relationships", "Social life", "Attitude"];
+  lineColors = ["rgba(0,0,255, .6)", "rgba(255,0,0, .6)", "rgba(128,0,128, .6)", "rgba(0,128,0, .6)", "rgba(255,165,0, .6)", "rgba(0,128,128, .6)", "rgba(255,0,255, .6)", "rgba(0,255,0, .6)"];
   constructor(public navCtrl: NavController, public navParams: NavParams, public chartProvider: ChartProvider) {
+    
   }
   history = [
     {
       date: "11/19/2018",
-      value: [0,10,10,10,10,10,10,10]
+      value: [0,10,2,1,4,5,6,9]
     },
     {
       date: "11/18/2018",
-      value: [8,8,8,8,8,8,8,8]
+      value: [8,2,3,5,4,10,9,7]
+    },
+    {
+      date: "11/17/2018",
+      value: [0,10,2,1,4,5,6,9]
+    },
+    {
+      date: "11/16/2018",
+      value: [8,2,3,5,4,10,9,7]
+    },
+    {
+      date: "11/15/2018",
+      value: [0,10,2,1,4,5,6,9]
+    },
+    {
+      date: "11/14/2018",
+      value: [8,2,3,5,4,10,9,7]
+    },
+    {
+      date: "11/13/2018",
+      value: [0,10,2,1,4,5,6,9]
+    },
+    {
+      date: "11/12/2018",
+      value: [8,2,3,5,4,10,9,7]
     }
   ]
-  dates = this.history.map(day=>{
-    return day.date
-  })
-
+  dates = this.history.map(day => day.date).sort()
+  values = (index) => {
+    let value = this.history.map(val => val.value[index])
+    return value
+  }
+  
   lineChartData = {
-    labels: ["11/19/2018", "11/18/2018","11/19/2018","11/19/2018","11/19/2018","11/19/2018","11/19/2018","11/19/2018"],
-    datasets:[{
-      label: "career",
-      data: [0,10,1,9,2,8,3,7],
-      backgroundColor: "rgba(255,153,0,0.4)"
-    }, {
-      label: "finance",
-      data: [8,8,7,5,3,4,5,6],
-      backgroundColor: "rgba(153,255,0,0.4)"
-    }]
+    labels: [],
+    datasets:[]
   }
   lineChartOptions = {
     elements: {
@@ -58,26 +77,46 @@ export class HistoryPage {
     },
       scales: {
       xAxes:[{
+        stacked: true
+        }
+      ],
+      yAxes: [{
         ticks: {
           min: 0,
           max: 10,
           beginAtZero: true,
           maxTicksLimit: 10,
-          stepSize: 1
+          stepSize: 1,
         }
       }]
     }
   }
-
-  ngOnInit() {
+  generateChart = () => {
     let ctx = this.lineChart.nativeElement;
     const myLineChart = new Chart(ctx, {
       type:'line',
       data: this.lineChartData,
       options: this.lineChartOptions
     })
-    
+    this.lineChartData.labels.push(...this.dates)
+    for(let i = 0; i < 8; i++){
+      let item = {
+      label: this.labels[i],
+      data: this.values(i),
+      borderColor: this.lineColors[i]
+      }
+      this.lineChartData.datasets.push(item)
+    }
     myLineChart.update()
+  }
+
+  async ngOnInit() {
+    
+    this.generateChart()
+    
+    console.log(this.lineChartData)
+    console.log(this.lineChartData.datasets)
+
   }
 
 
