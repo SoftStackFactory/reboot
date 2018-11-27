c80ed69d43e9d8ebc8f92f115e01cb245b0e6ceb
BrettWray <brett@brettwray.com>
2018-11-26 18:15:27 -0800

    Fixed merge blocking error

--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ChartProvider } from '../../providers/chart/chart';
 import { Chart } from 'chart.js';
