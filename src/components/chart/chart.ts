b8b9f07bf2218ef26f5f804bbd84f37fe4faa486
BrettWray <brett@brettwray.com>
2018-12-11 19:31:08 -0800

    Cleaned up the API call and the response, added comments.

--- src/components/chart/chart.ts
+++ src/components/chart/chart.ts
@@ -17,28 +17,18 @@ export class ChartComponent {
 
   @ViewChild('canvas') canvas;
 
-  /* These @Input variables are essential to being able to customize the charts based on what page is displaying it.
-     When you include a <chart> tag onto any page, you can now include these variables as attributes. For example:
-     <chart [pageData] = "chartProvider.assessmentChartData" [belongsTo] = "transitionPage"></chart>
-  */
-  @Input() pageData: any; //All chart data lives on the chart provider file and is passed in with this variable.
-  @Input() belongsTo: any; // Mostly just used to differentiate if the component is on the history page or not.
   chart: any;
   chartValues: any;
   constructor(public chartProvider: ChartProvider) { 
-    //this subscribes to the provider and gets the data from the DB
-    this.chartProvider.getChartHistory().map((res:Response)=>{
-      //this gets the most recent self assessment
-       let recent =  res[0].data;
-        //this creates an array for the data from the DB
-       this.chartValues = [recent.Attitude, recent.Career, recent.Family, recent.Finance, recent.Health,recent['Personal Growth'], recent.Relationships, recent['Social Life'] ]
-       //this calls the function to build the chart once the data is ready.
-       this.buildChart(this.chartValues)
+    //this subscribes to the provider which gets the data from the DB
+    this.chartProvider.mostRecentData().subscribe(res=>{
+      //calls the function that draws the chart on the canvas within the .html file of the component taking the response from the provider as an argument.
+      this.buildChart(res)
     })
-    .subscribe(data=>{data})
+    
   }
 
-  //this builds the chart.
+  //this builds the chart, it takes one parameter, the parameter needs to be an array with a length of 8 numbers in order for the chart to draw correctly
   buildChart(data){
     this.chart = new Chart(this.canvas.nativeElement, {
       type: 'polarArea',
@@ -47,7 +37,8 @@ export class ChartComponent {
         {
           backgroundColor: ["rgba(0,0,255, .6)", "rgba(255,0,0, .6)", "rgba(128,0,128, .6)", "rgba(0,128,0, .6)", "rgba(255,165,0, .6)", "rgba(0,128,128, .6)", "rgba(255,0,255, .6)", "rgba(0,255,0, .6)"],
           borderColor: "black",
-          data:  data // This data changes depending on what page owns the component. 
+          //data is the argument passed into the function.
+          data:  data
         }
       ]
       },
@@ -83,7 +74,7 @@ export class ChartComponent {
         },
       }
     });
-    //This sets the ticks on the chart to always start at 0 and end at 10.
+    //This sets the ticks on the chart to always start at 0 and end at 10, utilizing the global API of the local chart.
     this.chart.config.options.scale.ticks.min = 0;
     this.chart.config.options.scale.ticks.max = 10;
     this.chart.update();
--- src/providers/chart/chart.ts
+++ src/providers/chart/chart.ts
@@ -3,17 +3,33 @@ import { Injectable } from '@angular/core';
 
 import { ENV } from '@app/env';
 
+
+
+
 @Injectable()
 export class ChartProvider {
-
+  
   allResults: any;
   chartSections: any;
-  mostRecentChart: any = [0, 0, 0, 0, 0, 0, 0, 0]; // Data for the chart on the dashboard page
-  requestUrl: string = ENV.url
+  requestUrl: string = ENV.url + '/charts?access_token=' + sessionStorage.getItem('token')
+  testChart: any = [];
   constructor(public http: HttpClient) { }
 
   getChartHistory() {
-    return this.http.get(this.requestUrl + '/charts?access_token=' + sessionStorage.getItem('token'))
+    return this.http.get(this.requestUrl).subscribe((res:any[])=>{
+      
+      return res
+    })
+  }
+  mostRecentData() {
+    //calls the API to get the assesments from the db
+    return this.http.get(this.requestUrl).map((res:any[])=>{
+      //Returns all data, recent gets the last or most recent item from the DB
+      let recent = res[res.length-1].data
+      //destructures the response object and places it into an array so that the chart can consume it.
+      let mostRecentChart = [recent.Career, recent.Finance, recent['Personal Growth'], recent.Health, recent.Family, recent.Relationships, recent["Social Life"], recent.Attitude]
+      return mostRecentChart
+    })
   }
 
   
