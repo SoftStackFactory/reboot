77314e57e87900ef491c14dfe97b9bec665f21ce
BrettWray <brett@brettwray.com>
2018-12-12 19:46:03 -0800

    fixed formatting bug, added comments

--- src/providers/chart/chart.ts
+++ src/providers/chart/chart.ts
@@ -16,16 +16,13 @@ export class ChartProvider {
   testChart: any = [];
   constructor(public http: HttpClient) { }
 
-  addAssessment(assessment) {
-    let token = window.sessionStorage.getItem("token");
-    return this.http.post(this.requestUrl + '/charts?access_token=ff' + token, assessment);
-  }
-
   getChartHistory() {
     return this.http.get(this.requestUrl).map((res:any[])=>{
+      //Initialized an array to store the values.
       let allData:any = []
       res.map(x=>{
-        allData.push({date: moment(x.date).format('MM/DD/YYYY'), value: [x.data.Career, x.data.Finance, x.data.PersonalGrowth, x.data.Health, x.data.Family, x.data.Relationships, x.data.SocialLife, x.data.Attitude]})
+        //Maps the response from the API to a format that the history page will accept.
+        allData.push({date: moment(x.date).format('MM/DD/YYYY'), value: [x.data.Career, x.data.Finance, x.data['Personal Growth'], x.data.Health, x.data.Family, x.data.Relationships, x.data['Social Life'], x.data.Attitude]})
         return allData
       })
       return allData
@@ -37,7 +34,7 @@ export class ChartProvider {
       //Returns all data, recent gets the last or most recent item from the DB
       let recent = res[res.length-1].data
       //destructures the response object and places it into an array so that the chart can consume it.
-      let mostRecentChart = [recent.Career, recent.Finance, recent.PersonalGrowth, recent.Health, recent.Family, recent.Relationships, recent.SocialLife, recent.Attitude]
+      let mostRecentChart = [recent.Career, recent.Finance, recent['Personal Growth'], recent.Health, recent.Family, recent.Relationships, recent['Social Life'], recent.Attitude]
       return mostRecentChart
     })
   }
