b0c32c9cc3d362a3a7e5faf5c684973a7f6cb2c0
BrettWray <brett@brettwray.com>
2018-12-12 21:51:26 -0800

    fixed merge errors

--- src/providers/chart/chart.ts
+++ src/providers/chart/chart.ts
@@ -27,7 +27,7 @@ export class ChartProvider {
       let allData:any = []
       res.map(x=>{
         //Maps the response from the API to a format that the history page will accept.
-        allData.push({date: moment(x.date).format('MM/DD/YYYY'), value: [x.data.Career, x.data.Finance, x.data['Personal Growth'], x.data.Health, x.data.Family, x.data.Relationships, x.data['Social Life'], x.data.Attitude]})
+        allData.push({date: moment(x.date).format('MM/DD/YYYY'), value: [x.data.Career, x.data.Finances, x.data.PersonalGrowth, x.data.Health, x.data.Family, x.data.Relationships, x.data.SocialLife, x.data.Attitude]})
         return allData
       })
       return allData
@@ -39,7 +39,7 @@ export class ChartProvider {
       //Returns all data, recent gets the last or most recent item from the DB
       let recent = res[res.length-1].data
       //destructures the response object and places it into an array so that the chart can consume it.
-      let mostRecentChart = [recent.Career, recent.Finance, recent['Personal Growth'], recent.Health, recent.Family, recent.Relationships, recent['Social Life'], recent.Attitude]
+      let mostRecentChart = [recent.Career, recent.Finances, recent.PersonalGrowth, recent.Health, recent.Family, recent.Relationships, recent.SocialLife, recent.Attitude]
       return mostRecentChart
     })
   }
