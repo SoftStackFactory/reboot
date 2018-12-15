30dc9730fd0823bf446439c17182b8c9e8fa2892
Peter Horton <phorton@softstackfactory.org>
2018-12-15 10:37:24 -0800

    removed history page module

--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { ChartProvider } from '../../providers/chart/chart';
 
 /**
@@ -9,7 +9,6 @@ import { ChartProvider } from '../../providers/chart/chart';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
 @Component({
   selector: 'page-history',
   templateUrl: 'history.html',
