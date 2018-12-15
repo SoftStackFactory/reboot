b2590ad48bfe05e7315fb629b125a537135943b9
Peter Horton <phorton@softstackfactory.org>
2018-12-14 17:13:34 -0800

    added ionic forums solution for network provider

--- src/providers/network/network.ts
+++ src/providers/network/network.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Network } from '@ionic-native/network';
 import { Subscription } from 'rxjs';
-import { ToastController } from 'ionic-angular';
+import { AlertController, Events } from 'ionic-angular';
 
 /*
   Generated class for the NetworkProvider provider.
@@ -20,7 +20,32 @@ export enum ConnectionStatusEnum {
 @Injectable()
 export class NetworkProvider {
 
+  previousStatus;
 
+  constructor(public alertCtrl: AlertController, 
+              public network: Network,
+              public eventCtrl: Events) {
+
+    console.log('Hello NetworkProvider Provider');
+
+    this.previousStatus = ConnectionStatusEnum.Online;
+    
+  }
+
+    public initializeNetworkEvents(): void {
+        this.network.onDisconnect().subscribe(() => {
+            if (this.previousStatus === ConnectionStatusEnum.Online) {
+                this.eventCtrl.publish('network:offline');
+            }
+            this.previousStatus = ConnectionStatusEnum.Offline;
+        });
+        this.network.onConnect().subscribe(() => {
+            if (this.previousStatus === ConnectionStatusEnum.Offline) {
+                this.eventCtrl.publish('network:online');
+            }
+            this.previousStatus = ConnectionStatusEnum.Online;
+        });
+    }
 
 
 
