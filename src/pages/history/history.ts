e1db31b0bc5c2938fcae28b324d025164179f129
Robert Brown <robertlb4@gmail.com>
2018-12-17 12:28:23 -0800

    fix missing imports to test live deploy round 2

--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { ChartProvider } from '../../providers/chart/chart';
 import { Chart } from 'chart.js';
