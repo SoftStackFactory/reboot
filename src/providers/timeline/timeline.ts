1050ae6130b06e8fbb48ae64b28886a7f2235812
joey atwell <joeyatwell@gmail.com>
2018-12-17 08:24:34 -0800

    cleaning up code

--- src/providers/timeline/timeline.ts
+++ src/providers/timeline/timeline.ts
@@ -14,24 +14,24 @@ import { ENV } from '@app/env';
 export class TimelineProvider {
 
   requestUrl: string = "https://joey-new-heroku.herokuapp.com/api";
-
+  link: string = "this.requestUrl + '/appUsers/' + window.sessionStorage.getItem('userId') + '/timelines'"
 
   constructor(public http: HttpClient) {
     console.log('Hello TimelineProvider Provider');
   }
 
   saveTimeline(timeCompList, hasEntry: boolean = false){
-    const link = this.requestUrl + '/appUsers/' + window.sessionStorage.getItem('userId') + '/timelines';
+    // const link = this.requestUrl + '/appUsers/' + window.sessionStorage.getItem('userId') + '/timelines';
     if (hasEntry) {
-      return this.http.put(link, timeCompList); 
+      return this.http.put(this.link, timeCompList); 
     } else {
-      return this.http.post(link, timeCompList);
+      return this.http.post(this.link, timeCompList);
     }
   }
 
   getTimeline(){
     console.log("hasTimeline");
-    return this.http.get(this.requestUrl + '/appUsers/' + window.sessionStorage.getItem('userId') + '/timelines');
+    return this.http.get(this.link);
 
   } 
  
