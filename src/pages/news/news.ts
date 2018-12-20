ffb91ea7a9bbfc80f2543495bb8f00ec7a802f94
Peter Horton <phorton@softstackfactory.org>
2018-12-20 10:35:10 -0800

    removed news pages module

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { RssProvider } from '../../providers/rss/rss';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
 
-@IonicPage()
 @Component({
   selector: 'page-news',
   templateUrl: 'news.html',
