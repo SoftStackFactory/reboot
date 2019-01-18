89f5b836d72a52b57790e7d93e81cdfc351d6aaa
Gino17 <galentro451@gmail.com>
2019-01-17 17:26:03 -0800

    added functional progress bar

--- src/components/progress-bar/progress-bar.ts
+++ src/components/progress-bar/progress-bar.ts
@@ -1,22 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
-/**
- * Generated class for the ProgressBarComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
 @Component({
   selector: 'progress-bar',
   templateUrl: 'progress-bar.html'
 })
 export class ProgressBarComponent {
 
-  text: string;
+  @Input('progress') progress;
 
   constructor() {
-    console.log('Hello ProgressBarComponent Component');
-    this.text = 'Hello World';
+    
   }
 
 }
