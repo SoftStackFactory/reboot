0106f7339f6f6a8d48fc018f8f55a006518b2ee3
AlexRingrose <ringrose.alex@gmail.com>
2019-03-06 14:11:29 -0800

    Sort main resources array, removed second array & code formating

--- src/pages/resources/resources.ts
+++ src/pages/resources/resources.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {  NavController, NavParams, ModalController } from 'ionic-angular';
+import { NavController, NavParams, ModalController } from 'ionic-angular';
 import { UserProvider } from '../../providers/user/user';
 import { SelfAssessmentPage } from '../self-assessment/self-assessment';
 import { DashboardPage } from '../dashboard/dashboard';
@@ -11,16 +11,15 @@ interface UserData {
 }
 
 
-@Component({
+@Component( {
   selector: 'page-resources',
   templateUrl: 'resources.html',
-})
+} )
 export class ResourcesPage {
   //user model to be pushed to
   user: any = {};
   values: Array<any> = [];
   assessmentCompleted: boolean = false;
-  sortedResources: Array<any> = [];  
 
   //Resource tempate resources data
   resources: any = [
@@ -35,7 +34,7 @@ export class ResourcesPage {
       title: 'Finances',
       message: 'This is not about how much you have but rather how you relate to money. There are some millionaires who worry themselves sick about money so might score very low here and others who have just enough to get by and simply go with the flow.',
       modalMessage: 'This is not about how much you have but rather how you relate to money. There are some millionaires who worry themselves sick about money so might score very low here and others who have just enough to get by and simply go with the flow.',
-      score: 0      
+      score: 0
     },
     {
       title: 'Personal Growth',
@@ -53,7 +52,7 @@ export class ResourcesPage {
       title: 'Family',
       message: 'Family life is very important. Although family life should be loving, caring and supportive it often is not for a variety of reasons such as crying babies, wild teenage sons or daughters or constant arguments and tension.',
       modalMessage: 'Family life is very important. Although family life should be loving, caring and supportive it often is not for a variety of reasons such as crying babies, wild teenage sons or daughters or constant arguments and tension.',
-      score: 0      
+      score: 0
     },
     {
       title: 'Relationships',
@@ -71,72 +70,70 @@ export class ResourcesPage {
       title: 'Attitude',
       message: 'Attitude is vital and can influence every aspect of your life. Is your glass half full or half empty?',
       modalMessage: 'Attitude is vital and can influence every aspect of your life. Is your glass half full or half empty?',
-      score: 0  
+      score: 0
     }
   ];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
+  constructor ( public navCtrl: NavController, public navParams: NavParams,
     public modalCtrl: ModalController,
-    public userPro: UserProvider) {
+    public userPro: UserProvider ) {
     this.userPro.getUser()
-      .subscribe((data: UserData) => {
-        console.log(data)
+      .subscribe( ( data: UserData ) => {
         this.user.firstName = data.firstName;
         this.user.lastName = data.lastName;
-      });
+      } );
     this.userPro.getUserChart()
-    .subscribe((data: Array<any>) => {
-      try{
-        console.log(data);
-        this.resources.forEach(x => x.score = data['0'].data[x.title]);
-        this.sortedResources = this.resources.sort((a, b) => a.score - b.score);
-        console.log(data);
-        this.values = data[data.length - 1].data;
-        let lowest = 10;
-        let lowestProp: any;
-        console.log("values",this.values);
-        console.log(lowest); 
-        for (var prop in this.values) {
-          if (this.values[prop] < lowest) {
-            lowest = this.values[prop];
-            lowestProp = prop;
+      .subscribe( ( data: Array<any> ) => {
+        try {
+          this.resources
+            .forEach( x => x.score = data[ '0' ].data[ x.title ] );
+
+          this.resources = this.resources
+            .sort( ( a, b ) => a.score - b.score );
+
+          this.values = data[ data.length - 1 ].data;
+          let lowest = 10;
+          let lowestProp: any;
+
+          for ( var prop in this.values ) {
+            if ( this.values[ prop ] < lowest ) {
+              lowest = this.values[ prop ];
+              lowestProp = prop;
+            }
           }
-            console.log(lowest, lowestProp);
-      }
-      this.userPro.userData.lowestScore = lowest;
-      this.userPro.userData.lowScoreName = lowestProp;
-      this.userPro.updateUserModel(this.userPro.userData)
-      console.log(this.values)
-      this.assessmentCompleted = true;
-      console.log(data);
-    } catch(e) {
-      console.log('i ran')
-      return 
-    }
-  });
+          this.userPro.userData.lowestScore = lowest;
+          this.userPro.userData.lowScoreName = lowestProp;
+          this.userPro.updateUserModel( this.userPro.userData )
+
+          this.assessmentCompleted = true;
+        } catch ( e ) {
+          console.log( 'i ran' )
+          return
+        }
+      } );
   }
 
 
-  openModal(data) {
-    const resModal = this.modalCtrl.create('ResourceModalPage', { data: data })
+  openModal ( data ) {
+    const resModal = this.modalCtrl.create( 'ResourceModalPage', { data: data } )
 
     resModal.present();
   }
 
-  openSelfAssessment() {
-    this.navCtrl.setRoot(SelfAssessmentPage);
+  openSelfAssessment () {
+    this.navCtrl.setRoot( SelfAssessmentPage );
   }
 
-  openChartHistory() {
-    this.navCtrl.setRoot(HistoryPage);
+  openChartHistory () {
+    this.navCtrl.setRoot( HistoryPage );
   }
 
-  openDashboard() {
-    this.navCtrl.setRoot(DashboardPage);
+  openDashboard () {
+    this.navCtrl.setRoot( DashboardPage );
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ResourcesPage');
+  ionViewDidLoad () {
+    console.log( 'ionViewDidLoad ResourcesPage' );
   }
 
 }
